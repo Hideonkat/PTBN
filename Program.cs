d4463fa17dc1bd111e77e2a2a1cea0059fb01b49
Hideonkat <danghung0711dh@gmail.com>
2023-12-11 15:02:44 +0700

    ptbn

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ptbn;
+
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Phuong trinh bac nhat");
+        Console.WriteLine("ax+b=0, hay nhap a,b");
+        Console.Write("a:");
+        Double a = Double.Parse(Console.ReadLine());
+        Console.Write("b:");
+        Double b = Double.Parse(Console.ReadLine());
+        
+        if (a != 0)
+        {
+            Console.WriteLine("Phuong trinh co nghiem:" + -b/a);
+
+        }
+        else 
+        {
+            if(b==0)
+            {
+                Console.WriteLine("Vo so nghiem");
+
+            }
+            else
+            {
+                Console.WriteLine("Vo nghiem");
+            }
+
+            
+        }
+
+
+
+
+        }
+}
